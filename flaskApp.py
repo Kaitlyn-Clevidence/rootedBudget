427f8d31909e3cdb097f70c722de1392b5642e43
Kaitlyn-Clevidence <kclevidence@unr.edu>
2025-05-05 12:47:30 -0700

    updated dockerfile

--- flaskApp.py
+++ flaskApp.py
@@ -295,4 +295,4 @@ def teardown_db(exception):
     db.close_connection()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
